857afee993af6cfb93a04147fbdb578e631a685f
vayd0 <kiiroxyz@gmail.com>
2023-03-13 00:52:18 +0100

    Astral

---
+++ public/script.js
@@ -0,0 +1,40 @@
+const $card = document.querySelectorAll('.card');
+let bounds;
+
+
+
+$card.forEach((card) => {
+    console.log(card)
+    function rotateToMouse(e) {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        const leftX = mouseX - bounds.x;
+        const topY = mouseY - bounds.y;
+        const center = {
+            x: leftX - bounds.width / 2,
+            y: topY - bounds.height / 2
+        }
+        const distance = Math.sqrt(center.x ** 2 + center.y ** 2);
+
+        card.style.transform = `
+    scale3d(1.07, 1.07, 1.07)
+    rotate3d(
+      ${center.y / 100},
+      ${-center.x / 100},
+      0,
+      ${Math.log(distance) * 2}deg
+    )
+  `;
+    }
+    card.addEventListener('mouseenter', () => {
+        bounds = card.getBoundingClientRect();
+        document.addEventListener('mousemove', rotateToMouse);
+    });
+    card.addEventListener('mouseleave', () => {
+        document.removeEventListener('mousemove', rotateToMouse);
+        card.style.transform = '';
+        card.style.background = '';
+    });
+})
+
+
